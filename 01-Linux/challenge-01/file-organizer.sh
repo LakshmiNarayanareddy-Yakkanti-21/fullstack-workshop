dd6f18f0ec24323b2aa1c7b7e1e5183e6e16689a
LakshmiNarayanareddy-Yakkanti-21 <lnreddy2124@gmail.com>
2025-12-29 15:52:45 +0530

    added to the challenges

---
+++ 01-Linux/challenge-01/file-organizer.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; 
+then
+  echo "Usage: $0 <directory-path>"
+  exit 1
+fi
+
+FOLDER=$1
+
+if [ ! -d "$FOLDER" ]; 
+then
+  echo "Directory does not exist"
+  exit 1
+fi
+
+cd "$FOLDER"
+
+for FILE in *
+do
+  if [ -f "$FILE" ]; 
+then
+    EXTENSION="${FILE##*.}"
+
+    if [ ! -d "$EXTENSION" ]; 
+    then
+      mkdir "$EXTENSION"
+    fi
+
+    mv "$FILE" "$EXTENSION/"
+  fi
+done
+
+echo "Files organized successfully"
---
+++ 01-Linux/challenge-02/process-monitor.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+if [ $# -ne 2 ]; 
+then
+    echo "Usage: $0 <process-name> <interval-seconds>"
+    exit 1
+fi
+
+PROCESS_NAME=$1
+INTERVAL=$2
+
+echo "Monitoring process: $PROCESS_NAME"
+echo "Checking every $INTERVAL seconds..."
+echo "-----------------------------------"
+
+while true
+do
+    TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+    if pgrep "$PROCESS_NAME" > /dev/null
+    then
+        echo "[$TIMESTAMP] $PROCESS_NAME is running"
+    else
+        echo "[$TIMESTAMP] ALERT: $PROCESS_NAME is NOT running"
+        echo "Process stopped. Exiting monitor."
+        exit 1
+    fi
+
+    sleep $INTERVAL
+done
